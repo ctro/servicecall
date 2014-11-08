df982ddc906bb994742d4f7628cbe7f78d5f643c
ctro <clint@ctro.net>
2014-11-08 00:09:22 -0700

    Getting upcoming Workorders+Customers+Contact

--- Rakefile
+++ Rakefile
@@ -1,33 +1,37 @@
 # Main mail task
 require 'pony'
+require 'pp'
+require_relative './ls_api'
+
 namespace :mail do
 
   desc "Mail Service Reminders"
   task :service_reminder do
-    ENV["API_KEYS"].split(",").each do |key|
 
-      # TODO: put this somewhere better
-      if ENV["RACK_ENV"] == "production"
-        Pony.options = {
-          :via => :smtp,
-          :via_options => {
-            :address => 'smtp.sendgrid.net',
-            :port => '587',
-            :domain => 'heroku.com',
-            :user_name => ENV['SENDGRID_USERNAME'],
-            :password => ENV['SENDGRID_PASSWORD'],
-            :authentication => :plain,
-            :enable_starttls_auto => true
-          }
+    # TODO: put this somewhere better
+    if ENV["RACK_ENV"] == "production"
+      Pony.options = {
+        :via => :smtp,
+        :via_options => {
+          :address => 'smtp.sendgrid.net',
+          :port => '587',
+          :domain => 'heroku.com',
+          :user_name => ENV['SENDGRID_USERNAME'],
+          :password => ENV['SENDGRID_PASSWORD'],
+          :authentication => :plain,
+          :enable_starttls_auto => true
         }
-      end
-
-      Pony.mail(:to => key, :from => key,
-      :subject => 'Bring yer bike in',
-      :body => 'Sometime.')
-
-      puts "Sent mail to #{key}"
+      }
     end
+
+    # Right now defaults to HUB
+    l = LsAPI.new
+    alerts = l.work_alerts_for_upcoming_days(3)
+
+    Pony.mail(:to => "clint@ctro.net", :from => "clint@ctro.net",
+    :subject => 'I tink these people are coming in the next 3 days',
+    :body => pp(alerts))
+
   end
 
 end
--- ls_api.rb
+++ ls_api.rb
@@ -1,37 +1,44 @@
-module LsAPI
+class LsAPI
   require 'net/https'
   require 'json'
+  require 'time'
 
-  # Temp.
-  def self.hub_key
-    ENV["REAL_HUB_KEY"]
+  WorkAlert = Struct.new(:name, :email, :phone, :time_in)
+
+  def initialize(key=ENV["REAL_HUB_KEY"])
+    @key = key
+    @account_id = get('.json')["Account"]["accountID"]
   end
 
-  def self.get(path, qs, key=hub_key)
+  def work_alerts_for_upcoming_days(n_days)
+    today = Time.now.utc.iso8601
+    the_future = (Time.now.utc + (n_days*24*60*60)).iso8601
+
+    # %3E%3C = <>
+    get("Workorder.json?timeIn=%3E%3C,#{today},#{the_future}")["Workorder"].map do |wo|
+
+      cust = get("Customer/#{wo["customerID"]}.json?load_relations=all")["Customer"]
+      fname = cust['firstName']
+      email = cust['Contact']['Emails']['ContactEmail']['address']
+      phone = cust['Contact']['Phones']['ContactPhone']['number']
+
+      WorkAlert.new(fname, email, phone, wo['timeIn'])
+    end
+  end
+
+  private
+
+  def get(path)
     http = Net::HTTP.new("api.merchantos.com", 443)
     http.use_ssl = true
 
     response = http.start do |http|
-      request = Net::HTTP::Get.new("/API/#{path}.json?#{qs}")
-      request.basic_auth(key, 'apikey')
+      request = Net::HTTP::Get.new("/API/Account/#{@account_id}/#{path}")
+      request.basic_auth(@key, 'apikey')
       http.request(request)
     end
 
     JSON.parse(response.body)
-  end
-
-  def self.account_id(key=hub_key)
-    return get("Account", "", key)["Account"]["accountID"]
-  end
-
-  def self.workorders(key=hub_key)
-    return get("Account/#{account_id}/Workorder", "", key)["Workorder"]
-  end
-
-  # TODO: Figure out how to query for dates
-  def self.upcoming_workorders(key=hub_key)
-    # Time.now.utc.iso8601
-    return get("Account/#{account_id}/Workorder", "timeIn>2010", key)["Workorder"]
   end
 
 end
