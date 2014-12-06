c9dd56b8c426dd923b0443f9508ecbd9afded176
ctro <clint@ctro.net>
2014-12-06 16:31:02 -0700

    Add endpoint for testing API

--- servicecall.rb
+++ servicecall.rb
@@ -12,3 +12,21 @@ get '/' do "Litespeed ServiceCall" end
 
 # Monitor:
 get '/ping' do "PONG" end
+
+
+get '/reminder' do
+  email = params[:email]
+  days_out = params[:days_out]
+
+  l = LsAPI.new
+  alerts = l.work_alerts_for_upcoming_days(days_out.to_i)
+
+  Log.green("Sending alerts...")
+
+  message = Pony.mail(:to => email, :from => email,
+  :subject => "I think these people are coming in the next #{days_out} days",
+  :body => alerts)
+
+  Log.green(message.to_s)
+  "OK"
+end
